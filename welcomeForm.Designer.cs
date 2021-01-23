b73cfdb3616f197c9f27456d95b98472a97d13c3
Kiran <kiranjeetkaur509@gmail.com>
2021-01-23 21:23:42 +1300

    Add project files.

---
+++ welcomeForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace simpleGame
+{
+    partial class welcomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(welcomeForm));
+            this.start_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // start_btn
+            // 
+            this.start_btn.BackColor = System.Drawing.Color.Maroon;
+            this.start_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start_btn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.start_btn.Location = new System.Drawing.Point(438, 428);
+            this.start_btn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(592, 87);
+            this.start_btn.TabIndex = 0;
+            this.start_btn.Text = "START THE GAME";
+            this.start_btn.UseVisualStyleBackColor = false;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // welcomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.start_btn);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "welcomeForm";
+            this.Text = "welcome";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_btn;
+    }
+}
+
